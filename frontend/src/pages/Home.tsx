62e8a84424920d8386f51d5b389f5835f4097b03
Lucius Chee <lucius@czlucius.dev>
2024-01-06 19:15:50 +0800

    Home Page

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+
+export const Home = () => {
+    return <div className="bg-transparent">
+        <h1 className="fw-bold">Welcome to ClassTrivia!</h1>
+        <p>A class quiz platform for students. A gamified learning environment.</p>
+    </div>
+}
