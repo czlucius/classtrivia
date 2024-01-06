3c6b3fbb3874d930f37c2b56a949c0760b18d485
Lucius Chee <lucius@czlucius.dev>
2024-01-06 19:15:41 +0800

    NavBar

---
+++ frontend/src/components/Navigation.tsx
@@ -0,0 +1,26 @@
+import {Container, Nav, Navbar} from "react-bootstrap";
+import "./navigation.css"
+export const Navigation = () => {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary border border-2 m-2 rounded rounded-4">
+            <Container className="w-100">
+                <Navbar.Brand href="/" className=""><b>ClassTrivia</b></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="/auth">Login/Register</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export const Nav2 = () => {
+    return <div
+        className="main-nav"
+        style={{display: "flex", flex: "auto", flexDirection: "row", justifyContent: "space-between"}}>
+
+        ClassTrivia
+    </div>
+}
