5d9019f71583809ee250c5f7be7cf6bcbbf2f3c7
Lucius Chee <lucius@czlucius.dev>
2024-01-14 21:59:42 +0800

    Init backend

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+app.use(bodyParser.json())
+
+
+
+
+
+
+
+
+const PORT = 4567
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
+})
