a0549b9ebe353d59bb2053fb316a3dd4fdb11b8c
Lucius Chee <lucius@czlucius.dev>
2024-01-19 12:21:47 +0800

    Not found and Auth pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import './App.css'
 import {createBrowserRouter, Route, RouterProvider, Routes} from "react-router-dom";
 import {Home} from "./pages/Home.tsx";
 import {Navigation} from "./components/Navigation.tsx";
+import {NotFound404} from "./pages/NotFound404.tsx";
+import {Auth, AuthRegister} from "./pages/Auth.tsx";
 
 
 const router = createBrowserRouter([
@@ -13,10 +15,12 @@ function App() {
 }
 
 function Root() {
-    return <div >
+    return <div   style={{height: "100vh"}}>
         <Navigation/>
         <Routes>
+            <Route path="*" element={<NotFound404/>}/>
             <Route path="/" element={<Home/>}/>
+            <Route path="/auth" element={<Auth/>}/>
         </Routes>
     </div>
 }
