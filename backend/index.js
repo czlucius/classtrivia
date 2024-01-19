910cf21357e2e70c2ba68d30f76f0f84ce6fcf43
Lucius Chee <lucius@czlucius.dev>
2024-01-19 09:04:31 +0800

    Connect to MongoDB via Mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 const dotenv = require("dotenv")
 dotenv.config()
 
+const mongoose = require('mongoose');
+console.log(process.env.MONGODB_URL)
+mongoose.connect(process.env.MONGODB_URL, {
+    dbName: "classTrivia"
+})
 const express = require("express")
 const bodyParser = require("body-parser")
 const {authRouter} = require("./auth/users")
