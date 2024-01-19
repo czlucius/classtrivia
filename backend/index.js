ade81c561d4b7b589dd96afe0d9ac4bde34a6f4c
Lucius Chee <lucius@czlucius.dev>
2024-01-19 09:04:15 +0800

    Auth router

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,19 @@
 const dotenv = require("dotenv")
 dotenv.config()
+
 const express = require("express")
 const bodyParser = require("body-parser")
+const {authRouter} = require("./auth/users")
 const app = express()
+
 app.use(bodyParser.json())
 
 
 
 
+app.use("/api/auth", authRouter)
 
-
-
-
-const PORT = 4567
+const PORT = 9109
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
 })
