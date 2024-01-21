023da67f34b0ce26c45a0c90b416a89f927fe7f4
javin141 <66424374+javin141@users.noreply.github.com>
2024-01-21 14:31:05 +0800

    Schema for quizzes

---
+++ backend/data/quizzes.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const quizzesSchema = new mongoose.Schema({
+  quizId: String,
+  questions: [{
+    id: String,
+    type: String, // Multiple Choice (MCQ), Multi Select (MS)
+    score: Number // Marks awarded for correct answer
+    choices: [{
+      value: String,
+      correct: Boolean
+    }],
+
+  }]
+});
+
+const Quizzes = mongoose.model('Quizzes', quizzesSchema);
+
+module.exports = Quizzes;
