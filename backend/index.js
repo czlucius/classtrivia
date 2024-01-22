55791e9a06fdb66d1084348172b183e028156c1d
Lucius Chee <lucius@czlucius.dev>
2024-01-23 04:17:54 +0800

    Socket.IO (and possibly Redis)

--- backend/index.js
+++ backend/index.js
@@ -12,15 +12,36 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const {authRouter} = require("./auth/users")
 const app = express()
+const http = require("node:http")
+const {io} = require("./io/socket");
+const httpServer = http.createServer(app)
+
+const {createClient} = require("redis")
+const redisClient = createClient({
+    url: "redis-14278.c252.ap-southeast-1-1.ec2.cloud.redislabs.com:14278"
+})
+
+const cookieParser = require("cookie-parser")
+
+redisClient.on("connect", () => {
+    console.log("Connected to Redis!")
+})
+
+
 
 app.use(bodyParser.json())
-
-
-
-
+app.use(cookieParser())
 app.use("/api/auth", authRouter)
 
+
+io.on("connection", (socket) => {
+    socket.emit("hello", {message: "Hello World!"})
+})
+
+
 const PORT = 9109
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     console.log(`Listening on ${PORT} 🚀`)
 })
+
+module.exports = {httpServer, redisClient}
