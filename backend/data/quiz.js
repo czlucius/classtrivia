40c1edd61e6f4d4747cb17576ee071d0d9b3e18e
Lucius Chee <lucius@czlucius.dev>
2024-01-23 04:18:45 +0800

    Mongoose schema for questions

---
+++ backend/data/quiz.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const quizSchema = new mongoose.Schema({
+    _id: String,
+    questions: [{
+        id: String,
+        type: String,
+        photo: String,
+        correctAnswer: [{
+            description: String,
+            correct: Boolean
+        }],
+        value: Number,
+        data: [],
+        score: Number,
+        seconds: Number
+    }]
+});
+
+quizSchema.set("toJSON", (doc, rto) => {
+    rto.id = rto._id.toString()
+    delete rto.hashedPw
+    delete rto.__v
+})
+const Quiz = mongoose.model('Quiz', quizSchema);
+module.exports = Quiz
