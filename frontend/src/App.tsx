67e33a9c0d644e73008636f5b126bbfea5f76a3c
Lucius Chee <lucius@czlucius.dev>
2024-01-25 10:14:12 +0800

    New routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import {Home} from "./pages/Home.tsx";
 import {Navigation} from "./components/Navigation.tsx";
 import {NotFound404} from "./pages/NotFound404.tsx";
 import {Auth, AuthRegister} from "./pages/Auth.tsx";
+import {RegistrationComplete} from "./pages/RegistrationComplete.tsx";
+import {JoinQuiz} from "./pages/JoinQuiz.tsx";
 
 
 const router = createBrowserRouter([
@@ -21,6 +23,8 @@ function Root() {
             <Route path="*" element={<NotFound404/>}/>
             <Route path="/" element={<Home/>}/>
             <Route path="/auth" element={<Auth/>}/>
+            <Route path="/signup-complete" element={<RegistrationComplete/>}/>
+            <Route path="/quiz" element={<JoinQuiz/>}/>
         </Routes>
     </div>
 }
