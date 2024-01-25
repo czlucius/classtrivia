dd8f42c613cd759bc5d4b270e64c1d91d79e2d47
javin141 <66424374+javin141@users.noreply.github.com>
2024-01-25 10:51:13 +0800

    Update quizController.js

--- backend/quizcreation/quizController.js
+++ backend/quizcreation/quizController.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const quizRouter = express.Router(); // Use quizRouter 
-const Quizzes = require('../data/quizzes'); // pathj for schema
+const Quizzes = require('../data/quiz'); // pathj for schema
 
 quizRouter.post('/create-quiz', express.json(), async (req, res) => {
   try {
     const quizData = req.body;
-    const newQuiz = new Quizzes(quizData);
+    const newQuiz = new Quiz(quizData);
     const savedQuiz = await newQuiz.save();
     res.status(201).json(savedQuiz);
   } catch (error) {
