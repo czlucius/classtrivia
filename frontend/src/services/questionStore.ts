e740a1b1ec28fa694380e5b25b1ae3828e0c8bfa
Lucius Chee <lucius@czlucius.dev>
2024-01-31 23:22:47 +0800

    Redux stores for questions and quizzes

---
+++ frontend/src/services/questionStore.ts
@@ -0,0 +1,37 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState: {
+    [id: string]: Question
+} = {
+
+}
+const questionSlice = createSlice({
+    name: 'question',
+    initialState: initialState,
+    reducers: {
+        /**
+         * Idempotent function to PUT a question.
+         */
+        putQuestion: (state, action) => {
+            const {id, question} = action.payload
+            state[id] = question
+        },
+        clearQuestions: (state, action) => {
+            state = {}
+        },
+        removeQuestion: (state, action) => {
+            const {id} = action.payload
+            window.URL.revokeObjectURL(state[id].image)
+            delete state[id]
+        },
+        updateInfo: (state, action) => {
+            const {title, desc} = action.payload
+            state["title"] = title
+            state["desc"] = desc
+        }
+    }
+});
+
+export const { putQuestion, removeQuestion, clearQuestions} = questionSlice.actions;
+
+export default questionSlice.reducer;
