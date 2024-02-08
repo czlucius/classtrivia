f3241c51c93d43ac0bfb16be8a53565d2bcce01b
Lucius Chee <lucius@czlucius.dev>
2024-02-08 14:09:00 +0800

    Functions for saving an entire quiz

---
+++ frontend/src/services/questionApi.ts
@@ -0,0 +1,44 @@
+import {Question} from "../Question.ts";
+
+export async function saveQuiz(q:  {[id: string]: Question}, title: string, description: string) {
+    // await fetch()
+    // Upload the image first
+
+    const questions = {...q}
+
+    const qnList = []
+    for (const questionId in questions) {
+        const question: Question = {...questions[questionId]}
+        if (question.image) {
+            const imageResponse = await fetch(question.image)
+            const imageBlob = await imageResponse.blob()
+
+            const response = await fetch("/api/storage/upload", {
+                headers: {
+                    "X-File-Name": `quizImages/${question.id}`
+                }, body: imageBlob,
+                method: "PUT"
+            })
+            const formatted = await response.json()
+            if (response.ok && !formatted.error) {
+                question.image = formatted.url
+            }
+
+        }
+        qnList.push(question)
+
+    }
+
+    const reqBody = {
+        title, description, questions: qnList
+    }
+    await fetch("/api/quiz/create-quiz", {
+        method: "POST",
+        body: JSON.stringify(reqBody),
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+    })
+
+}
